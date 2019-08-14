50b4eed3af316644fc165e89ffd4627ada394f43
Kafungo Wangure <kaf.wangure@gmail.com>
2019-08-14 21:13:48 +0300

    Accept flip attribute to icon.

--- src/main/components/Icon.svelte
+++ src/main/components/Icon.svelte
@@ -2,9 +2,10 @@
     export let icon
     export let fontSize = false
     export let spin = false
+    export let flip = false
 </script>
 
-<i class="mdi mdi-{icon} {spin ? 'mdi-spin': ''}" 
+<i class="mdi mdi-{icon} {spin ? 'mdi-spin': ''} {flip? `mdi-flip-${flip}` : ''}" 
     on:click on:dblclick
     style="{fontSize? `font-size: ${fontSize} px`: ''}"></i>
 
