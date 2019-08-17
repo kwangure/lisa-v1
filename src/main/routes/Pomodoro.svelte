00c5c63d95e8a288b4c8aed81b3e4fe39312f611
Kafungo Wangure <kaf.wangure@gmail.com>
2019-08-17 19:00:34 +0300

    Move timer component into Pomodoro page.

--- src/main/routes/Pomodoro.svelte
+++ src/main/routes/Pomodoro.svelte
@@ -1,11 +1,11 @@
 <script>
-    import { TimerState, Phase } from '../../background/Timer';
-    import { OptionsClient, SettingsClient, PomodoroClient } from '../../background/Services';
-    import { clamp, mmss } from '../../Filters';
+    import { TimerState, Phase } from '../../background/Timer'
+    import { clamp, mmss } from '../../Filters'
+    import { onDestroy, onMount } from 'svelte'
+    import { OptionsClient, SettingsClient, PomodoroClient } from '../../background/Services'
     import M from '../../Messages';
     import Button from '../components/Button.svelte'
-    import Timer from '../components/Timer.svelte'
-    import { onDestroy, onMount } from 'svelte'
+    import Icon from '../components/Icon.svelte'
 
     let elapsed = null
     let state = null
@@ -39,26 +39,44 @@
         
     })()
 
+    onMount(()=>{
+        document.addEventListener('keydown', onKeyDown);
+    })
+
     onDestroy(() => {
         clearInterval(timeInterval)
         pomodoroClient.dispose()
+        document.removeEventListener('keydown', onKeyDown);
     })
 
-    $: remaining = duration - elapsed
+    $: checkpointStartAt && updateElapsed()
+    $: checkpointElapsed && updateElapsed()
+    $: duration && updateElapsed()
 
-    $:  remainingSeconds = Math.ceil(remaining)
-
-    $: elapsedSeconds = Math.ceil(elapsed)
-    
+    $: isRunning = state == TimerState.Running;
+    $: isPaused = state == TimerState.Paused;
+    $: isStopped = state == TimerState.Stopped
     $: hasTime = duration != null &&
             checkpointStartAt != null &&
             checkpointElapsed != null;
 
-    $: checkpointStartAt && updateElapsed()
+    $: remaining = duration - elapsed
+    $: remainingSeconds = Math.ceil(remaining)
+    $: elapsedSeconds = Math.ceil(elapsed)
+    $: time = (()=> {
+        if (!hasTime) {
+            return '––:––';
+        }
+        let remaining = Math.max(0, Math.ceil(duration - elapsed));
+        return mmss(remaining);
+    })()
 
-    $: checkpointElapsed && updateElapsed()
-    
-    $: duration && updateElapsed()
+    $: timerClass = {
+            null: '',
+            [Phase.Focus]: 'focus',
+            [Phase.ShortBreak]: 'break',
+            [Phase.LongBreak]: 'break'
+        }[phase];
     
     $: (function(to) {
         clearInterval(timeInterval);
@@ -88,22 +106,6 @@
         }
     }
 
-    onMount(()=>{
-        document.addEventListener('keydown', onKeyDown);
-    })
-
-    onDestroy(()=>{
-        document.removeEventListener('keydown', onKeyDown);
-    })
-    
-    function showSettings() {
-        OptionsClient.once.showPage('settings');
-    }
-
-    function showHistory() {
-        OptionsClient.once.showPage('history');
-    }
-
     function onKeyDown(e) {
         if (e.key != " ") {
             return;
@@ -125,44 +127,47 @@
     function onRestart() {
         PomodoroClient.once.restart();
     }
-  
-    $: timerClass = {
-            null: '',
-            [Phase.Focus]: 'focus',
-            [Phase.ShortBreak]: 'break',
-            [Phase.LongBreak]: 'break'
-        }[phase];
 
-    async function saveState(to) {
-        if (to != TimerState.Stopped) {
-            return;
-        }
-        
-        let settings = await SettingsClient.once.getSettings();
-        let { countdown } = settings[{
-            [Phase.Focus]: 'focus',
-            [Phase.ShortBreak]: 'shortBreak',
-            [Phase.LongBreak]: 'longBreak'
-        }[phase]];
-        if (countdown.autoclose) {
-            //window.close();
-        }
+    function showSettings() {
+        OptionsClient.once.showPage('settings');
     }
-    
-    $: saveState(state)
+
+    function showHistory() {
+        OptionsClient.once.showPage('history');
+    }
 </script>
 
 <div>
     <div class="countdown">
-        <Timer
-            class={timerClass}
-            state={state}
-            duration={duration}
-            elapsed={elapsed}
-            enabled={hasTime}
-            pause={onPause}
-            resume={onResume}
-            restart={onRestart}/>
+        <div class="timer {timerClass}">
+            <div class="overlay">
+                <div class="controls">
+                    {#if isPaused}
+                        <Button on:click={onRestart} icon="restart">
+                            {M.restart_timer}
+                        </Button>
+                    {:else}
+                        <Button class="placeholder">Placeholder</Button>
+                    {/if}  
+                </div>
+                <div class="time" class:enabled={hasTime} class:paused={isPaused}>
+                    { time }
+                </div>
+                <div class="controls">
+                    {#if isRunning}
+                        <Button on:click={onPause} icon="pause">
+                            {M.pause_timer}
+                        </Button>
+                    {:else if isPaused}
+                        <Button on:click={onResume} icon="play">
+                            {M.resume_timer}
+                        </Button>
+                    {:else}
+                        <Button class="placeholder">Placeholder</Button>
+                    {/if}
+                </div>
+            </div>
+        </div>
     </div>
     <Button on:click={showSettings} icon="settings-outline" class="settings">
         <span>{ M.settings }</span>
@@ -203,15 +208,79 @@
         display: inherit;
         }
     }
-
     :global(.settings) {
         left: 30px;
         bottom: 50px;
     }
-
     :global(.history) {
         display: none;
         left: 40px;
         bottom: 50px;
+    }
+    @keyframes blink  {
+        0% {
+            opacity: 1;
+        }
+        49% {
+            opacity: 1;
+        }
+        50% {
+            opacity: 0;
+        }
+        100% {
+            opacity: 0;
+        }
+    }
+    .timer {
+        height: 100%;
+        width: 100%;
+        position: relative;
+    }
+    .timer svg {
+        width: 100%;
+        height: 100%;
+    }
+    .timer svg path.duration {
+        stroke: #aaa;
+        stroke-width: 3px;
+        fill: none;
+    }
+    .timer svg path.elapsed {
+        stroke-width: 3px;
+        stroke-linecap: round;
+        fill: none;
+    }
+    .overlay {
+        position: absolute;
+        left: 0;
+        top: 0;
+        bottom: 0;
+        right: 0;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+    .overlay .time {
+      font-weight: 500;
+      color: rgba(255, 255, 255, 0.65);
+      font-size: 30px;
+    }
+    .enabled {
+        color: #333;
+    }
+    .paused {
+        animation: blink 1s linear infinite;
+    }
+    .controls {
+        z-index: 2;
+        margin: 3vmin 0;
+    }
+    .controls svg {
+        width: 7vmin;
+        height: 7vmin;
+    }
+    :global(.placeholder){
+        visibility: hidden;
     }
 </style>
