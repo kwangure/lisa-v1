870885504c2aca7ce120fa3d5a830c18f1c57a26
kwangure <kaf.wangure@gmail.com>
2019-08-17 19:02:58 +0300

    Chore: Remove unnecessary comments.

--- src/main/Application.js
+++ src/main/Application.js
@@ -4,19 +4,6 @@ import mapRoutes from './mapRoutes'
 
 export default class Application {
     constructor(){
-        /**
-         * A map of routes, keyed by a unique route name. Each route is an object
-         * containing the following properties:
-         *
-         * - `path` The path that the route is accessed at.
-         * - `component` The Mithril component to render when this route is active.
-         *
-         * @example
-         * app.routes.home = {path: '/d/:id', component: IndexPage};
-         *
-         * @type {Object}
-         * @public
-         */
         this.routes = {};
 
         loadRoutes(this)
