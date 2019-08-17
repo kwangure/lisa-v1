777bc25770944c894304a1938867570c3916e5fd
kwangure <kaf.wangure@gmail.com>
2019-08-17 19:03:58 +0300

    Chore: Remove unused route.

--- src/main/routes.js
+++ src/main/routes.js
@@ -1,15 +1,10 @@
 import Pomodoro from './routes/Pomodoro.svelte'
-import Profile from './routes/Profile.svelte'
 
 export default function (app) {
     app.routes = {
         'index': { 
             path: '/', 
             component: Pomodoro
-        },
-        'profile': {
-            path: '/profile', 
-            component: Profile
         },
     }
 }
