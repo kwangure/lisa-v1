3b9d22c61bf818a03c89bbd977723799e83ffa20
Kafungo Wangure <kaf.wangure@gmail.com>
2019-08-28 13:05:59 +0300

    chore: remove unused route.

--- src/options/routes.js
+++ src/options/routes.js
@@ -1,15 +1,10 @@
 import Settings from './routes/Settings.svelte'
-import History from './routes/History.svelte'
 
 export default function (app) {
     app.routes = {
         'index': { 
             path: '/', 
             component: Settings
-        },
-        'profile': {
-            path: '/profile', 
-            component: History
         },
     }
 }
