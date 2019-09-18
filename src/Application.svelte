46ecc17ffbd452333b7fba1daede2dedde4da9fc
Kafungo Wangure <kaf.wangure@gmail.com>
2019-09-18 20:54:14 +0300

    refactor: start the beginnings of a state machine

---
+++ src/Application.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { pomodoroService } from './TimerMachine.js'
+
+    pomodoroService.start()
+</script>
+
+<style>
+    /* your styles go here */
+</style>
+
+<!-- markup (zero or more items) goes here -->
