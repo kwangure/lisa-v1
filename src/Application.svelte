4761a067ee8eb8ce239907ecd3e1ef0f133725ce
kwangure <kaf.wangure@gmail.com>
2019-09-20 12:14:05 +0300

    refactor: one step closer

--- src/Application.svelte
+++ src/Application.svelte
@@ -1,11 +1,35 @@
 <script>
     import { pomodoroService } from './TimerMachine.js'
 
+    let context = null     
+
+    pomodoroService.onTransition(state => {
+        if (state.changed) {
+            context = state
+        }
+    });
+
     pomodoroService.start()
+
+    function start(){
+        pomodoroService.send('START')
+    }
+
+    function advance(){
+        pomodoroService.send('NEXT_PHASE')
+    }
 </script>
+
+<div>
+    <button on:click={start}>
+        ACTIVATE
+    </button>
+    <button on:click={advance}>
+        NEXT_PHASE
+    </button>
+
+</div>
 
 <style>
     /* your styles go here */
 </style>
-
-<!-- markup (zero or more items) goes here -->
