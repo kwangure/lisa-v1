cfa5dd3a1894a67c4c1be7e2aa880bbac98f9298
Kafungo Wangure <kaf.wangure@gmail.com>
2019-10-09 10:18:53 +0300

    fix(background): handle chrome runtime lasterror

--- src/background/emmitter_observer.js
+++ src/background/emmitter_observer.js
@@ -1,15 +1,19 @@
 function emit_tabs(message){
     if(chrome.tabs){
-        chrome.tabs.query({}, function(tabs){
+        chrome.tabs.query({ active: true }, function(tabs){
             for (let i = 0, len = tabs.length; i < len; i++) {
-            chrome.tabs.sendMessage(tabs[i].id, message, {}, ()=>{});
+                chrome.tabs.sendMessage(tabs[i].id, message, {}, ()=>{
+                    chrome.runtime.lastError
+                });
             }
         })
     }
 }
 
 function emit_runtime(message) {
-    chrome.runtime.sendMessage(message);
+    chrome.runtime.sendMessage(message, () => {
+        chrome.runtime.lastError
+    });
 }
 
 export default function (event) {
