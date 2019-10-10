294db5a26ab6eb1087f871525cbca8b1c65d826b
Kafungo Wangure <kaf.wangure@gmail.com>
2019-10-11 00:19:40 +0300

    refactor(content): switch from a svelte timer component to store

---
+++ src/content/timer_store.js
@@ -0,0 +1,43 @@
+
+import { writable } from "svelte/store";
+import emit from "../background/emitter_observer"
+import { events } from "../background/pomodoro_store"
+
+let pomodoro = writable({});
+
+async function respond(message, sender, sendResponse) {
+    pomodoro.set(message)
+    sendResponse({});
+    return true;
+}
+
+chrome.runtime.onMessage.addListener(respond);
+
+window.onclose = () => chrome.runtime.onMessage.removeListener(respond);
+
+export const timer_actions = {
+    start: function () {
+        emit(events.START)
+    },
+    stop: function () {
+        emit(events.STOP)
+    },
+    pause: function () {
+        emit(events.PAUSE)
+    },
+    resume: function () {
+        emit(events.RESUME)
+    }
+}
+
+/*function restart_timer() {
+    
+}
+
+function restart_cycle() {
+    
+}*/
+
+export function timer_readable() {
+    return { subscribe: pomodoro.subscribe }
+}
