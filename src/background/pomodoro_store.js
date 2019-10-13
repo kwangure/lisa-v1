724cd7947b2e46d6e318e2eb7db9839596e86f32
Kafungo Wangure <kaf.wangure@gmail.com>
2019-10-13 04:18:50 +0300

    fix(background): correct issue with focus onstop

--- src/background/pomodoro_store.js
+++ src/background/pomodoro_store.js
@@ -300,7 +300,7 @@ export function pomodoro_store(timer, settings_readable){
                 status.elapsed = 0;
                 status.pomodoros_since_start = 0;
                 status.state = states.STOPPED; 
-                state.phase = phases.FOCUS;
+                status.phase = phases.FOCUS;
                 return status
             })
             this.unsubscribe_from_timer()
@@ -345,6 +345,7 @@ export function pomodoro_store(timer, settings_readable){
         },
         tick: function () {
             pomodoro_store.update(status => {
+                debugger;
                 switch (status.state) {
                     case states.PAUSED:
                         return status;
