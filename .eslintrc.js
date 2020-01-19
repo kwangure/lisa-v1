41999d294243705e14e053accbcd9343f3422649
kafungo <kaf.wangure@gmail.com>
2020-01-19 18:30:15 +0300

    chore: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+    },
+    "plugins": [
+        "svelte3",
+    ],
+    "extends": [
+        "eslint:recommended",
+    ],
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "modules": true,
+        }
+    },
+    "overrides": [
+        {
+            "files": ['**/*.svelte'],
+            "processor": 'svelte3/svelte3'
+        }
+    ],
+    "settings": {
+        "svelte3/ignore-styles": () => true,
+
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "comma-dangle": ["error", "always-multiline"],
+    }
+};
