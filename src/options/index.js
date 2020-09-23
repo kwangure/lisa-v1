fd79ad0cbf5445bd7b386d6c403dbc1cb455064f
kwangure <kaf.wangure@gmail.com>
2020-09-24 00:11:06 +0300

    feat: import global css

--- src/options/index.js
+++ src/options/index.js
@@ -1,5 +1,6 @@
 import root from "./_root.svelte";
 import routes from "./routes/index.js";
 import router from "@deimimi/svelte-pagejs-router";
+import "@deimimi/strawberry/css/shared.css";
 
 router(root, routes, { hashbang: true });
