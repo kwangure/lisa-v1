dcae5cc5b1411facc75f1c37d615d94d8139690b
kwangure <kaf.wangure@gmail.com>
2020-09-23 19:57:38 +0300

    feat: make milliseconds to time conversion dynamic

--- src/utils/time.js
+++ src/utils/time.js
@@ -2,10 +2,20 @@ export function minutesToMilliseconds(mins) {
     return mins * 60 * 1000;
 }
 
-export function millisecondsToHumanReadableTime(milliseconds) {
+export function millisecondsToHumanReadableTime(milliseconds, options = {}) {
+    let {
+        minutesBuilder = (minutes) => minutes === 0 ? "" : `${minutes}`,
+        secondsBuilder = (seconds) => seconds === 0 ? "" : `${seconds}`,
+        timeTemplate = "$[minutes]:$[seconds]",
+    } = options;
     const MILLIS_IN_SECOND = (1000);
     const MILLIS_IN_MINUTE = (60 * MILLIS_IN_SECOND);
-    const minutes = String(Math.floor(milliseconds / MILLIS_IN_MINUTE)).padStart(2, "0");
-    const seconds = String(Math.floor((milliseconds % MILLIS_IN_MINUTE)/ MILLIS_IN_SECOND)).padStart(2, "0");
-    return `${minutes}:${seconds}`;
+
+    const minutes = Math.floor(milliseconds / MILLIS_IN_MINUTE);
+    const seconds = Math.floor((milliseconds % MILLIS_IN_MINUTE)/ MILLIS_IN_SECOND);
+
+    timeTemplate = timeTemplate.replace("$[minutes]", minutesBuilder(minutes));
+    timeTemplate = timeTemplate.replace("$[seconds]", secondsBuilder(seconds));
+
+    return timeTemplate;
 }
