644d8d60157c22ed7917fc8b8ac98733c153c6ac
kwangure <kaf.wangure@gmail.com>
2020-09-24 16:06:05 +0300

    feat: simplify time formatting

--- src/utils/time.js
+++ src/utils/time.js
@@ -2,20 +2,17 @@ export function minutesToMilliseconds(mins) {
     return mins * 60 * 1000;
 }
 
-export function millisecondsToHumanReadableTime(milliseconds, options = {}) {
-    let {
-        minutesBuilder = (minutes) => minutes === 0 ? "" : `${minutes}`,
-        secondsBuilder = (seconds) => seconds === 0 ? "" : `${seconds}`,
-        timeTemplate = "$[minutes]:$[seconds]",
-    } = options;
+function formatTime({ minutes, seconds }) {
+    seconds = String(seconds).padStart(2, "0");
+    return `${minutes}:${seconds}`;
+}
+
+export function millisecondsToHumanReadableTime(milliseconds, formatter = formatTime) {
     const MILLIS_IN_SECOND = (1000);
     const MILLIS_IN_MINUTE = (60 * MILLIS_IN_SECOND);
 
     const minutes = Math.floor(milliseconds / MILLIS_IN_MINUTE);
     const seconds = Math.floor((milliseconds % MILLIS_IN_MINUTE)/ MILLIS_IN_SECOND);
 
-    timeTemplate = timeTemplate.replace("$[minutes]", minutesBuilder(minutes));
-    timeTemplate = timeTemplate.replace("$[seconds]", secondsBuilder(seconds));
-
-    return timeTemplate;
+    return formatter({ minutes, seconds });
 }
