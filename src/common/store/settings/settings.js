afe184010427793e236545c093a2d2d0ef37d1da
kwangure <kaf.wangure@gmail.com>
2020-10-04 02:00:45 +0300

    refactor: use event packaged event listener

--- src/common/store/settings/settings.js
+++ src/common/store/settings/settings.js
@@ -1,12 +1,11 @@
 import { readable } from "svelte/store";
-import { settings, EventListener } from "../../events";
+import { settings } from "../../events";
 
 export async function createSettingsWritable() {
     const initialSettings = await settings.getState();
 
     const settingsReadable = readable(initialSettings, (setReadableValue) => {
-        const settingsEventsListener = new EventListener("BACKGROUND.SETTINGS");
-        const unsubscribe = settingsEventsListener.on("CHANGED", (settings) => {
+        const unsubscribe = settings.on("CHANGED", (settings) => {
             setReadableValue(settings);
         });
         return unsubscribe;
