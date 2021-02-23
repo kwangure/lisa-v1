bd2b29589c894ca45aacc1ec76ce8cad1a65cafe
kwangure <kaf.wangure@gmail.com>
2021-02-23 21:09:31 +0300

    fix: update label to new Strawberry API

--- src/options/routes/appearance.svelte
+++ src/options/routes/appearance.svelte
@@ -5,8 +5,8 @@
     export let settingStore;
 </script>
 
-<Select bind:value={$settingStore.appearanceSettings.timerPosition}
-    label="Set timer position">
+<Select bind:value={$settingStore.appearanceSettings.timerPosition}>
+    <span slot="label">Set timer position</span>
     {#each Object.values(timerPositions) as { value, name }}
         <Option {value}>{name}</Option>
     {/each}
