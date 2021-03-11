390e12babac2de90039af0d436beca4e98daf561
kwangure <kaf.wangure@gmail.com>
2021-03-11 12:29:15 +0300

    fix: upgrade @kwangure eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,1 @@
-const config = require("@kwangure/eslint-config-svelte");
-
-// Work around https://github.com/babel/babel/issues/12985
-delete config.rules["template-curly-spacing"];
-delete config.rules["indent"];
-
-module.exports = config;
+module.exports = require("@kwangure/eslint-config-svelte");
