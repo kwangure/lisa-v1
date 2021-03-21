ce8cebbb581c11351b360d4a04962aa46a408cf1
kwangure <kaf.wangure@gmail.com>
2021-03-21 20:35:11 +0300

    chore: finally learn how to use eslint properly

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,32 @@
-const config = require("@kwangure/eslint-config-svelte");
-const path = require("path");
-
-config.globals = {
-    ...config.globals,
-    chrome: false,
-};
-
-config.rules["max-len"] = ["error", {
-    code: 100, // xState objects are painful without this
-    ignoreComments: true,
-}];
-config.rules["id-denylist"] = config
-    .rules["id-denylist"]
-    .filter((r) => r !== "data");
-
-config.plugins.push("import");
-config.plugins = [...(new Set(config.plugins))];
-
-config.settings = {
-    ...config.settings,
-    "import/resolver": {
-        alias: {
-            map: [
-                ["~@phase_ui", path.resolve(__dirname, "./src/common/phase_ui")],
-                ["~@common", path.resolve(__dirname, "./src/common/")],
-                ["~@content", path.resolve(__dirname, "./src/content/")],
-                ["~@popup", path.resolve(__dirname, "./src/popup/")],
-                ["~@utils", path.resolve(__dirname, "./src/utils/")],
-            ],
-            extensions: [".js", ".svelte", ".json"],
+module.exports = {
+    globals: {
+        chrome: false,
+    },
+    extends: [
+        "@kwangure/eslint-config-svelte",
+    ],
+    plugins: [
+        "import",
+    ],
+    settings: {
+        "import/resolver": {
+            alias: {
+                map: [
+                    ["~@phase_ui", "./src/common/phase_ui"],
+                    ["~@common", "./src/common/"],
+                    ["~@content", "./src/content/"],
+                    ["~@popup", "./src/popup/"],
+                    ["~@utils", "./src/utils/"],
+                ],
+                extensions: [".js", ".svelte", ".json"],
+            },
         },
     },
+    rules: {
+        "max-len": ["error", {
+            code: 100, // xState objects are painful without this
+            ignoreComments: true,
+        }],
+        "id-denylist": ["error", "err", "e", "cb", "callback"],
+    },
 };
-
-module.exports = config;
